96e38ce71b9e05e580b39981a5b6743b25b475cf
elenimingko <elenimingo889@yahoo.com>
2024-01-06 19:32:23 +0100

    newadd

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ function displa(val) {
 function cleare() {
     document.getElementById('display').value ='';
   }
-  
-  function calculatore() {
-   let resultat= document.getElementById('display').value;
-   let resul=eval(resultat);
-   document.getElementById('display').value=resul;
 
-  
+  function calculatore() {
+    let operators = ['+', '-', '*', '/'];
+   let number= document.getElementById('display').value;
+   const last = number[number.length - 1];
+   console.log(last);
+   if (operators.includes(last)) {
+    return ;
+   }else{
+     let result=eval(number);
+   document.getElementById('display').value=result;
+   }
   }
+
+
+ 
